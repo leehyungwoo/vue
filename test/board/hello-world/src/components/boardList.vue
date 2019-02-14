dc21a151740c1ba20f636b4753abaf7522dc92a9
dlguddn3214 <dlguddn3214@naver.com>
2019-02-15 03:15:42 +0900

    test

---
+++ test/board/hello-world/src/components/boardList.vue
@@ -0,0 +1,42 @@
+<style>
+table {
+  min-width: 320px;
+  width: 100%;
+  margin: 0 auto;
+}
+table,
+th,
+td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th,
+td {
+  padding: 5px;
+  text-align: left;
+}
+</style>
+
+<template>
+
+  <table>
+    <caption>board table</caption>
+    <tr>
+      <th>Month</th>
+      <th>Savings</th>
+    </tr>
+    <tr>
+      <td>January</td>
+      <td>$100</td>
+    </tr>
+    <tr>
+      <td>February</td>
+      <td>$50</td>
+    </tr>
+  </table>
+
+</template>
+
+<script>
+export default {};
+</script>
---
+++ test/board/hello-world/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <boardList></boardList>
+  </div>
+</template>
+
+<script>
+import boardList from "../components/boardList";
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  components: { boardList }
+};
+</script>
+
+
+<style>
+
+</style>

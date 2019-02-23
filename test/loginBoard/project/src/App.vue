194e51eeeb960fb34145f1a6addacb63d96c88af
dlguddn3214 <dlguddn3214@naver.com>
2019-02-23 13:41:03 +0900

    no message

---
+++ test/loginBoard/project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>

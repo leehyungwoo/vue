ccebc783423ff4c262748cf38cbf257b699d7a61
dlguddn3214 <dlguddn3214@naver.com>
2019-03-16 14:14:57 +0900

    로컬스토리지

--- memidev/16/fe/src/views/headers.vue
+++ memidev/16/fe/src/views/headers.vue
@@ -2,6 +2,10 @@
   <v-container>
     <v-btn @click="headerSend">헤더 전송</v-btn>
     <v-btn @click="headerSend2">임의헤더 전송</v-btn>
+    <v-btn @click="lsWrite">로컬스토리지 쓰기</v-btn>
+    <v-btn @click="lsRead">로컬스토리지 읽기</v-btn>
+    <v-btn @click="lsRemove">로컬스토리지 지우기</v-btn>
+    <v-btn @click="lsClear">로컬스토리지 비우기</v-btn>
   </v-container>
 </template>
 <script>
@@ -22,6 +26,19 @@ export default {
         })
         .then(r => console.log(r))
         .catch(e => console.log(e));
+    },
+    lsWrite() {
+      localStorage.setItem("token", 123);
+      localStorage.setItem("zzz", 333);
+    },
+    lsRead() {
+      console.log(localStorage.getItem("token"));
+    },
+    lsRemove() {
+      localStorage.removeItem("token");
+    },
+    lsClear() {
+      console.log(localStorage.clear());
     }
   }
 };

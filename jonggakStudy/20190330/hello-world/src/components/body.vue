ec18bc5cd642d73d4a30832b5b53351a89ca6943
dlguddn3214 <dlguddn3214@naver.com>
2019-03-30 15:11:36 +0900

    vue cli defaut

---
+++ jonggakStudy/20190330/hello-world/src/components/body.vue
@@ -0,0 +1,69 @@
+<template>
+  <div style="padding-top:30px;width:600px;padding-left:30px;">
+    <div style="font-weight: bold;margin-bottom: 3px;">회원 수 : {{ctodos.length}}</div>
+    <table class="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th>아이디</th>
+          <th>이름</th>
+          <th>상태</th>
+          <th>가입시간</th>
+          <th>탈퇴시간</th>
+          <th>기능</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(todo,index) in ctodos"
+          @click="detail(todo);"
+          :key="todo.pk"
+        >
+          <td>{{ todo.id }}</td>
+          <td>{{ todo.name }}</td>
+          <td>
+            <span v-if="todo.state == 1">정상</span>
+            <span v-else>탈퇴</span>
+          </td>
+          <td>{{ todo.RegTime }}</td>
+          <td>{{ todo.WdTime }}</td>
+          <td>
+            <button
+              v-if="todo.state == 1"
+              v-on:click="withdraw(todo)"
+            >탈퇴</button>
+            <button
+              v-else-if="todo.state == 0"
+              v-on:click="detailpoparray(index)"
+            >삭제</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bodyCom",
+  data() {
+    return {
+      ctodos: this.todos
+    };
+  },
+  props: ["todos"],
+  methods: {
+    detail(obj) {
+      if (event.target.nodeName == "BUTTON") return false;
+      this.$emit("updateobj", obj);
+    },
+    detailpoparray(idx) {
+      this.todos.splice(idx, 1);
+    },
+
+    withdraw(obj) {
+      this.$emit("withdraw", obj);
+    }
+  }
+};
+</script>
+ 
---
+++ jonggakStudy/20190330/hello-world/src/components/header.vue
@@ -0,0 +1,82 @@
+<template>
+  <div style="padding-top:30px;width:600px;padding-left:30px;">
+
+    <div>
+      <Label for="id">아이디</Label>
+      <input
+        type="text"
+        id="id"
+        v-model="m_obj.id"
+        @keypress.enter="pusharray"
+      />
+    </div>
+    <div style="margin: 7px 0px">
+      <Label for="name">이름</Label>
+      <input
+        type="text"
+        id="name"
+        v-model="m_obj.name"
+        @keypress.enter="pusharray"
+      />
+    </div>
+    <div style="margin-left: 50px">
+      <button
+        v-if="!this.uobj"
+        v-on:click="pusharray"
+      >추가</button>
+      <button
+        v-else
+        v-on:click="updatearray"
+      >수정</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "headerCom",
+
+  data() {
+    return {
+      m_obj: {
+        id: "",
+        name: ""
+      },
+      uobj: this.updateobj
+    };
+  },
+  props: {
+    updateobj: {
+      type: Object
+    }
+  },
+
+  methods: {
+    pusharray() {
+      if (!this.vail()) return false;
+      this.$emit("pusharray", this.m_obj);
+      this.mobjreset();
+    },
+    mobjreset() {
+      this.m_obj = { id: null, name: null };
+    },
+    vail() {
+      if (!this.m_obj.id || !this.m_obj.id.trim()) {
+        alert("아이디을 입력해 주세요.");
+        return false;
+      } else if (!this.m_obj.name || !this.m_obj.name.trim()) {
+        alert("아이디을 입력해 주세요.");
+        return false;
+      }
+      return true;
+    },
+    updatearray() {
+      if (this.obj) {
+        this.obj.id = this.updateobj.id;
+        this.obj.name = this.updateobj.name;
+      }
+    }
+  }
+};
+</script>
+ 

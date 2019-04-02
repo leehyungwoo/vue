dc485d8186a13c8c03ce056e57c0f0bfba7413e0
dlguddn3214 <dlguddn3214@naver.com>
2019-04-02 18:41:15 +0900

    test

---
+++ test/router/routertest/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <button @click="home">Home</button> |
+      <button @click="about">About</button>
+    </div>
+    <router-view />
+  </div>
+</template>
+ 
+
+ <script>
+export default {
+  methods: {
+    home() {
+      //   console.log("홈실행");
+      this.$router.push({ name: "home", params: { test: "params" } });
+    },
+    about() {
+      //   console.log("어바웃실행");
+      this.$router.push({ path: "about", query: { test: "query" } });
+    }
+  }
+};
+</script>
